34f5ad5e7ec63bd57ad3b6db5076064b2349b769
theobelo25 <96204247+theobelo25@users.noreply.github.com>
2026-01-30 15:56:19 -0500

    Enhance FeaturedWeddings to conditionally load films

--- app/(root)/featured-weddings.tsx
+++ app/(root)/featured-weddings.tsx
@@ -1,15 +1,21 @@
 import Carousel from "../../components/shared/carousel";
-import { getFeaturedFilms } from "@/lib/actions/film.actions";
+import { getFeaturedFilms, getFilms } from "@/lib/actions/film.actions";
 
-const FeaturedWeddings = async () => {
-  const featuredFilms = await getFeaturedFilms();
+const FeaturedWeddings = async ({ isFeatured }: { isFeatured: boolean }) => {
+  let films;
+  if (isFeatured) {
+    films = await getFeaturedFilms();
+  } else {
+    films = await getFilms();
+  }
 
   return (
     <section>
       <h2 className="relative h2-subheading mb-10">Featured Weddings</h2>
-      <Carousel posts={featuredFilms} />
+      <Carousel posts={films} />
     </section>
   );
 };
 
 export default FeaturedWeddings;
+
